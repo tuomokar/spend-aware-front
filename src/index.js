ffc7ca4abf0f1c52a81d50b1cf940bf70afc0963
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2017-01-14 22:35:26 +0200

    Page's height is the all that's available

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/Application';
+import 'styles/App.css';
+
 
 ReactDOM.render(<Application />, document.getElementById('root'));
