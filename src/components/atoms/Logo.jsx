202e3bb2618eb9027c5de49306a4491688e5aea7
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2017-01-15 10:00:30 +0200

    Header has a simple logo

---
+++ src/components/atoms/Logo.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite/no-important';
+
+import LeafImage from '../../images/leaf.png';
+import Image from 'components/atoms/Image';
+import Styles from 'constants/Styles';
+import Title from 'components/atoms/Title';
+
+/**
+ * A basic text component
+ */
+export default class Logo extends React.Component {
+
+    render() {
+        return (
+            <span className={ css(styles.logoContainer )}>
+                <Image height={ Styles.layout.headerHeight }>
+                    { LeafImage }
+                </Image>
+                <Title
+                    level={ 2 }
+                    display={'inline'}
+                    margin={ '0 0 0 15px'}>
+                    SpendAware - Wellness Through Awareness
+                </Title>
+            </span>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    logoContainer: {
+        marginLeft: '10%',
+        display: 'flex',
+        alignItems: 'center'
+    }
+})
--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, css } from 'aphrodite/no-important';
 
+import Logo from 'components/atoms/Logo';
 import Styles from 'constants/Styles.js';
 
 /**
@@ -10,18 +11,18 @@ export default class Application extends React.Component {
 
     render() {
         return (
-            <div className={ css(styles.root) }>
-                header will come here
+            <div className={ css(styles.headerContainer) }>
+                <Logo />
             </div>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    root: {
+    headerContainer: {
         backgroundColor: Styles.colors.headerColor,
         position: 'absolute',
         width: '100%',
-        height: Styles.layout.headerMargin
+        height: Styles.layout.headerHeight
     }
 });
